7ecaf7a901ccb944f9856a48848ab105e2aa1cf0
KangJuSung <kangju1996@naver.com>
2021-09-27 10:27:36 +0900

    주성

---
+++ app/src/main/java/com/smhrd/teamproject/LifePattern.java
@@ -0,0 +1,14 @@
+package com.smhrd.teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LifePattern extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_pattern);
+    }
+}
---
+++ app/src/main/java/com/smhrd/teamproject/Ragister_G.java
@@ -0,0 +1,14 @@
+package com.smhrd.teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ragister_G extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ragister_g);
+    }
+}
